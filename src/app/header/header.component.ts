b4c8264c9fb158dbf0b33d74f6903402a884b380
giusy03-cloud <giusyrizzo677@gmail.com>
2025-07-19 19:20:01 +0200

    modificaBarraMenu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { AuthService } from '../services/auth.service';
-import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
@@ -11,8 +11,26 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterModule],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  isLoggedIn = false;
+  isLoginPage = false;
+
   constructor(public authService: AuthService, private router: Router) {}
+
+  ngOnInit() {
+    this.checkState();
+
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.checkState();
+      }
+    });
+  }
+
+  checkState() {
+    this.isLoggedIn = this.authService.isAuthenticated();
+    this.isLoginPage = this.router.url.startsWith('/auth/login');
+  }
 
   logout() {
     this.authService.logout();
