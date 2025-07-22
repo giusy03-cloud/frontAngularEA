cc83f0b6bcd7ccaa9c5195b9038cac481d745941
Marika <marikafoti2017@gmail.com>
2025-07-22 16:00:12 +0200

    aggiunta profilo, devo aggiustare recensioni di eventi

---
+++ src/app/feature/profile/profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css'],
+  standalone: true,
+  imports: [CommonModule, RouterModule]
+})
+export class ProfileComponent {}
